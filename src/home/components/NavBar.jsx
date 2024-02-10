34679ac6dce6bbfec2529d6d5a36dee5d0106429
NachoFdev <nacho.fdev@gmail.com>
2024-02-10 15:18:59 +0100

    Scroll navbar home implemented

--- src/home/components/NavBar.jsx
+++ src/home/components/NavBar.jsx
@@ -1,18 +1,38 @@
 import { AppBar, IconButton, Toolbar } from '@mui/material';
 import { MenuOutlined } from '@mui/icons-material';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Slide from '@mui/material/Slide';
 
 
-export const NavBar = () => {
+function HideOnScroll(props) {
+  const { children, window } = props;
+
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
+
   return (
-    <AppBar
-      position='fixed'
-      sx={{  }}
-    >
-      <Toolbar>
-        <IconButton>
-          <MenuOutlined />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+
+export const NavBar = (props) => {
+  return (
+    <HideOnScroll {...props}>
+      <AppBar
+        position='fixed'
+        sx={{  }}
+      >
+        <Toolbar>
+          <IconButton>
+            <MenuOutlined />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
+    </HideOnScroll>
   );
 };
